008b685eed2340adde1fcce247537b4b50bbd037
kathy zhang <kzhang193@gmail.com>
2018-11-16 02:35:45 -0600

    changed structure of nested objected into an array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,103 +2,57 @@ var quizContent = [
 
     { 
         question: "Who were Harry Potter's parents?",
-        choices: {
-            a: "James and Lilly",
-            b: "Tom and Margaret",
-            c: "William and Mary",
-            d: "Henry and Elizabeth"
-        },
-        theAnswer: "a"
+        choices: ["James and Lilly","Tom and Margaret","William and Mary","Henry and Elizabeth"],
+        theAnswer: "James and Lilly"
     }, 
     
     {   
         question: "Which creatures guard Azkaban, the Wizarding prison?",
-        choices: {
-            a: "Animagi",
-            b: "Dementors",
-            c: "Death Eaters",
-            d: "Hippogriffs"
-        },
-        theAnswer: "b"
+        choices: ["Animagi","Dementors","Death Eaters","Hippogriffs"],
+        theAnswer: "Death Eaters"
     }, 
     
     {   
         question: "What is Lord Voldemort's true identity?",
-        choices: {
-            a: "Lucius Malfoy",
-            b: "Gellert Grindelwald",
-            c: "Tom Riddle",
-            d: "Sirius Black"
-        },
-        theAnswer: "c"
+        choices: ["Lucius Malfoy","Gellert Grindelwald", "Tom Riddle","Sirius Black"],
+        theAnswer: "Tom Riddle"
     }, 
     
     {   
         question: "Which of Harry's friends becomes a professor at Hogwarts?",
-        choices: {
-            a: "Hermione Granger",
-            b: "Cho Chang",
-            c: "Cedric Diggory",
-            d: "Neville Longbottom"
-        },
-        theAnswer: "d"
+        choices: ["Hermione Granger","Cho Chang","Cedric Diggory","Neville Longbottom"], 
+        theAnswer: "Neville Longbottom"
     }, 
     
     {   
         question: "What are the three Unforgiveable Curses?",
-        choices: {
-            a: "Avada Kedavra, Expecto Patronum, and Imperio",
-            b: "Expelliarmus, Obliviate, and Sectumsempra",
-            c: "Avada Kedavra, Crucio, and Imperio",
-            d: "Accio, Crucio, and Riddikulus"
-        },
-        theAnswer: "c"
+        choices: ["Avada Kedavra, Expecto Patronum, and Imperio",
+        "Expelliarmus, Obliviate, and Sectumsempra",
+        "Avada Kedavra, Crucio, and Imperio",
+        "Accio, Crucio, and Riddikulus"],
+        theAnswer: "Avada Kedavra, Crucio, and Imperio"
     }, 
     
     {   
         question: "Professor Lupin, Harry's 3rd Year Defense Against the Dark Arts teacher is revealed to be half... :",
-        choices: {
-            a: "Werewolf",
-            b: "Muggle",
-            c: "Vampire",
-            d: "Giant"
-        },
-        theAnswer: "a"
+        choices: ["Werewolf","Muggle","Vampire","Giant"],
+        theAnswer: "Werewolf"
     }, 
     
     {   
         question: "How many points are awarded in for catching the Golden Snitch?",
-        choices: {
-            a: "500",
-            b: "150",
-            c: "70",
-            d: "30"
-        },
-        theAnswer: "b"
+        choices: ["500","150","70","30"],
+        theAnswer: "150"
     }, 
     
     {   
         question: "What magical creature snuck into Hogwarts on Halloween during Harry's first year?",
-        choices: {
-            a: "Basilisk",
-            b: "Acromantula",
-            c: "Troll",
-            d: "Dragon"
-        },
-        theAnswer: "a"
+        choices: ["Basilisk","Acromantula","Troll","Dragon"],
+        theAnswer: "Troll"
     } 
-
-    
 
 ];
 
 
-
-
-////console.log(quizContent[3]); >>  the entire object
-////console.log(quizContent[3].question);  >>  "Which of Harry's friends becomes a professor at Hogwarts?"
-///console.log(quizContent[3]).choices); >> the object containing the choices for question 3
-///console.log(quizContent[3]).choices.c); >> the content of c: 
-///console.log(quizContent[3]).theAnswer); >> d 
 
 
