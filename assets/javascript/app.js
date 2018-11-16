d3eb147414e16556b406e6482cf9b3e49897405d
kathy zhang <kzhang193@gmail.com>
2018-11-15 23:40:16 -0600

    quiz content object

---
+++ assets/javascript/app.js
@@ -0,0 +1,84 @@
+var quizContent = [
+
+    {   question: "Is this my first question?",
+        choices: {
+            a: "choice1",
+            b: "choice2",
+            c: "choice3",
+            d: "choice4"
+        },
+        theAnswer: "choiceX"
+    }, 
+    
+    {   question: "Is this my first question?",
+        choices: {
+            a: "choice1",
+            b: "choice2",
+            c: "choice3",
+            d: "choice4"
+        },
+        theAnswer: "choiceX"
+    }, 
+    
+    {   question: "Is this my first question?",
+        choices: {
+            a: "choice1",
+            b: "choice2",
+            c: "choice3",
+            d: "choice4"
+        },
+        theAnswer: "choiceX"
+    }, 
+    
+    {   question: "Is this my first question?",
+        choices: {
+            a: "choice1",
+            b: "choice2",
+            c: "choice3",
+            d: "choice4"
+        },
+        theAnswer: "choiceX"
+    }, 
+    
+    {   question: "Is this my first question?",
+        choices: {
+            a: "choice1",
+            b: "choice2",
+            c: "choice3",
+            d: "choice4"
+        },
+        theAnswer: "choiceX"
+    }, 
+    
+    {   question: "Is this my first question?",
+        choices: {
+            a: "choice1",
+            b: "choice2",
+            c: "choice3",
+            d: "choice4"
+        },
+        theAnswer: "choiceX"
+    }, 
+    
+    {   question: "Is this my first question?",
+        choices: {
+            a: "choice1",
+            b: "choice2",
+            c: "choice3",
+            d: "choice4"
+        },
+        theAnswer: "choiceX"
+    }, 
+    
+    {   question: "Is this my first question?",
+        choices: {
+            a: "choice1",
+            b: "choice2",
+            c: "choice3",
+            d: "choice4"
+        },
+        theAnswer: "choiceX"
+    }
+    
+    
+];
